3c5b024bfa45b6ab7b280919ae270a4c6507b8eb
Johannes van Roest Dahl <65940893+joha2738@users.noreply.github.com>
2023-02-03 18:50:48 +0100

    Opdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -32,10 +32,14 @@ pins.servoWritePin(AnalogPin.P8, 89)
 let fugtmåler = smarthome.ReadSoilHumidity(AnalogPin.P1)
 basic.pause(1000)
 basic.forever(function () {
+	
+})
+loops.everyInterval(3600000, function () {
     fugtmåler = smarthome.ReadSoilHumidity(AnalogPin.P1)
     basic.showNumber(fugtmåler)
     basic.pause(1000)
-    if (fugtmåler < 85) {
+    if (fugtmåler < 65) {
+        basic.showIcon(IconNames.Sad)
         Servo(60)
         Pumpetid(2000)
         basic.pause(500)
@@ -45,6 +49,6 @@ basic.forever(function () {
         Servo(89)
         basic.clearScreen()
     } else {
-    	
+        basic.showIcon(IconNames.Happy)
     }
 })
